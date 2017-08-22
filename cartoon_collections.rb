9974105a8d7fde80167cd3d42d259c2d6a838606
Elbin Keeppanasseril <elbincodes@gmail.com>
2017-08-22 22:14:32 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)# code an argument here
   # Your code here
+  arr.each_with_index do |elem, i|
+    puts (i+1).to_s + " " + elem
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)# code an argument here
   # Your code here
+  arr.collect do |elem|
+    (elem.capitalize).concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
+
+
+def long_planeteer_calls(arr)# code an argument here
   # Your code here
+  arr.any? do |elem|
+    elem.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+
+def find_the_cheese(arr)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  arr.find do |elem|
+    cheese_types.include?(elem)
+    end
 end
